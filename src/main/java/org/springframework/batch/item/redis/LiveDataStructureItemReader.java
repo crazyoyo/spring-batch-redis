10533237eddacbbfe9ee2e0d35f4db4cc6328100
jruaux <jruaux@gmail.com>
2020-12-06 22:46:55 -0800

    removed pool config from live reader options

--- src/main/java/org/springframework/batch/item/redis/LiveDataStructureItemReader.java
+++ src/main/java/org/springframework/batch/item/redis/LiveDataStructureItemReader.java
@@ -1,22 +1,24 @@
 package org.springframework.batch.item.redis;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.batch.item.redis.support.AbstractKeyValueItemReader;
 import org.springframework.batch.item.redis.support.DataStructure;
 import org.springframework.batch.item.redis.support.DataStructureReader;
 import org.springframework.batch.item.redis.support.LiveKeyItemReader;
 import org.springframework.batch.item.redis.support.LiveReaderOptions;
-import org.springframework.batch.item.redis.support.RedisClientBuilder;
+import org.springframework.batch.item.redis.support.RedisConnectionPoolBuilder;
 
 import io.lettuce.core.AbstractRedisClient;
+import io.lettuce.core.api.StatefulConnection;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 public class LiveDataStructureItemReader extends AbstractKeyValueItemReader<DataStructure> {
 
-	public LiveDataStructureItemReader(AbstractRedisClient client, LiveReaderOptions options) {
+	public LiveDataStructureItemReader(AbstractRedisClient client,
+			GenericObjectPoolConfig<StatefulConnection<String, String>> poolConfig, LiveReaderOptions options) {
 		super(new LiveKeyItemReader(client, options.getLiveKeyReaderOptions()),
-				new DataStructureReader(client, options.getPoolConfig()), options.getTransferOptions(),
-				options.getQueueOptions());
+				new DataStructureReader(client, poolConfig), options.getTransferOptions(), options.getQueueOptions());
 	}
 
 	public static LiveDataStructureItemReaderBuilder builder() {
@@ -26,12 +28,12 @@ public class LiveDataStructureItemReader extends AbstractKeyValueItemReader<Data
 	@Setter
 	@Accessors(fluent = true)
 	public static class LiveDataStructureItemReaderBuilder
-			extends RedisClientBuilder<LiveDataStructureItemReaderBuilder> {
+			extends RedisConnectionPoolBuilder<LiveDataStructureItemReaderBuilder> {
 
 		private LiveReaderOptions options = LiveReaderOptions.builder().build();
 
 		public LiveDataStructureItemReader build() {
-			return new LiveDataStructureItemReader(client, options);
+			return new LiveDataStructureItemReader(client, poolConfig, options);
 		}
 
 	}
--- src/main/java/org/springframework/batch/item/redis/support/LiveReaderOptions.java
+++ src/main/java/org/springframework/batch/item/redis/support/LiveReaderOptions.java
@@ -1,8 +1,5 @@
 package org.springframework.batch.item.redis.support;
 
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-
-import io.lettuce.core.api.StatefulConnection;
 import lombok.Builder;
 import lombok.Builder.Default;
 import lombok.Data;
@@ -17,7 +14,5 @@ public class LiveReaderOptions {
 	private QueueOptions queueOptions = QueueOptions.builder().build();
 	@Default
 	private LiveKeyReaderOptions liveKeyReaderOptions = LiveKeyReaderOptions.builder().build();
-	@Default
-	private GenericObjectPoolConfig<StatefulConnection<String, String>> poolConfig = new GenericObjectPoolConfig<>();
 
 }
