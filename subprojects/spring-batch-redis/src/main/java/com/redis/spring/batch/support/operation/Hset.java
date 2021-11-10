25555f11683189219f0510fc86203c2c4ca62e30
Julien Ruaux <jruaux@gmail.com>
2021-11-10 13:44:01 -0800

    Added support for HDEL

--- subprojects/spring-batch-redis/src/main/java/com/redis/spring/batch/support/operation/Hset.java
+++ subprojects/spring-batch-redis/src/main/java/com/redis/spring/batch/support/operation/Hset.java
@@ -12,18 +12,25 @@ import io.lettuce.core.api.async.RedisHashAsyncCommands;
 
 public class Hset<K, V, T> extends AbstractKeyOperation<K, V, T> {
 
-	private final Converter<T, Map<K, V>> map;
+	private final Converter<T, Map<K, V>> mapConverter;
+	private final Predicate<T> hdel;
 
-	public Hset(Converter<T, K> key, Predicate<T> delete, Converter<T, Map<K, V>> map) {
+	public Hset(Converter<T, K> key, Predicate<T> delete, Converter<T, Map<K, V>> map, Predicate<T> hdel) {
 		super(key, delete);
 		Assert.notNull(map, "A map converter is required");
-		this.map = map;
+		this.mapConverter = map;
+		this.hdel = hdel;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	protected RedisFuture<Long> doExecute(BaseRedisAsyncCommands<K, V> commands, T item, K key) {
-		return ((RedisHashAsyncCommands<K, V>) commands).hset(key, map.convert(item));
+		RedisHashAsyncCommands<K, V> hashCommands = ((RedisHashAsyncCommands<K, V>) commands);
+		Map<K, V> map = mapConverter.convert(item);
+		if (hdel.test(item)) {
+			return hashCommands.hdel(key, (K[]) map.keySet().toArray());
+		}
+		return hashCommands.hset(key, map);
 	}
 
 	public static <K, V, T> HsetMapBuilder<K, V, T> key(Converter<T, K> key) {
@@ -47,6 +54,7 @@ public class Hset<K, V, T> extends AbstractKeyOperation<K, V, T> {
 
 		private final Converter<T, K> key;
 		private final Converter<T, Map<K, V>> map;
+		private Predicate<T> hdel = t -> false;
 
 		public HsetBuilder(Converter<T, K> key, Converter<T, Map<K, V>> map) {
 			super(map);
@@ -54,9 +62,14 @@ public class Hset<K, V, T> extends AbstractKeyOperation<K, V, T> {
 			this.map = map;
 		}
 
+		public HsetBuilder<K, V, T> hdel(Predicate<T> hdel) {
+			this.hdel = hdel;
+			return this;
+		}
+
 		@Override
 		public Hset<K, V, T> build() {
-			return new Hset<>(key, del, map);
+			return new Hset<>(key, del, map, hdel);
 		}
 	}
 
