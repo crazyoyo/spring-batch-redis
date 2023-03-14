bb12c830853e8e92fd5b9059876ad7239b0adb30
Julien Ruaux <jrx@github.com>
2023-03-14 01:14:04 -0400

    test: Added modules to enterprise database

--- subprojects/spring-batch-redis/src/test/java/com/redis/spring/batch/RedisEnterpriseModulesTests.java
+++ subprojects/spring-batch-redis/src/test/java/com/redis/spring/batch/RedisEnterpriseModulesTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.condition.OS;
 import org.springframework.util.unit.DataSize;
 
 import com.redis.enterprise.Database;
+import com.redis.enterprise.RedisModule;
 import com.redis.testcontainers.RedisEnterpriseContainer;
 import com.redis.testcontainers.RedisServer;
 import com.redis.testcontainers.RedisStackContainer;
@@ -14,7 +15,8 @@ class RedisEnterpriseModulesTests extends AbstractModulesTests {
 
 	protected static final RedisEnterpriseContainer source = new RedisEnterpriseContainer(
 			RedisEnterpriseContainer.DEFAULT_IMAGE_NAME.withTag("latest"))
-			.withDatabase(Database.name("BatchTests").memory(DataSize.ofMegabytes(50)).ossCluster(true).build());
+			.withDatabase(Database.name("BatchTests").memory(DataSize.ofMegabytes(50)).ossCluster(true)
+					.modules(RedisModule.JSON, RedisModule.TIMESERIES).build());
 
 	private static final RedisStackContainer target = new RedisStackContainer(
 			RedisStackContainer.DEFAULT_IMAGE_NAME.withTag(RedisStackContainer.DEFAULT_TAG));
