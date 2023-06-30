ef76317e28eec24cb15be2feb75928cd4cfe04b0
Julien Ruaux <jruaux@users.noreply.github.com>
2023-06-30 10:16:48 -0700

    refactor: Cleaned up operation

--- subprojects/spring-batch-redis/src/main/java/com/redis/spring/batch/writer/DataStructureWriteOperation.java
+++ subprojects/spring-batch-redis/src/main/java/com/redis/spring/batch/writer/DataStructureWriteOperation.java
@@ -3,8 +3,10 @@ package com.redis.spring.batch.writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
+import java.util.function.Function;
 
 import com.redis.lettucemod.timeseries.AddOptions;
+import com.redis.lettucemod.timeseries.AddOptions.Builder;
 import com.redis.spring.batch.common.BatchOperation;
 import com.redis.spring.batch.common.DataStructure;
 import com.redis.spring.batch.common.Operation;
@@ -35,27 +37,30 @@ public class DataStructureWriteOperation<K, V> implements BatchOperation<K, V, D
 	private StreamIdPolicy streamIdPolicy = DEFAULT_STREAM_ID_POLICY;
 
 	private final Operation<K, V, DataStructure<K>, Object> noop = new Noop<>();
-	private final Hset<K, V, DataStructure<K>> hset = new Hset<>(DataStructure::getKey, DataStructure::getValue);
-
-	private final Set<K, V, DataStructure<K>> set = new Set<>(DataStructure::getKey, DataStructure::getValue);
-
-	private final JsonSet<K, V, DataStructure<K>> jsonSet = new JsonSet<>(DataStructure::getKey,
-			DataStructure::getValue);
-
-	private final RpushAll<K, V, DataStructure<K>> rpush = new RpushAll<>(DataStructure::getKey,
-			DataStructure::getValue);
-
-	private final SaddAll<K, V, DataStructure<K>> sadd = new SaddAll<>(DataStructure::getKey, DataStructure::getValue);
-
-	private final ZaddAll<K, V, DataStructure<K>> zadd = new ZaddAll<>(DataStructure::getKey, DataStructure::getValue);
-
-	private final TsAddAll<K, V, DataStructure<K>> tsAdd = new TsAddAll<>(DataStructure::getKey,
-			DataStructure::getValue, AddOptions.<K, V>builder().build());
-
+	private final Hset<K, V, DataStructure<K>> hset = new Hset<>(key(), value());
+	private final Set<K, V, DataStructure<K>> set = new Set<>(key(), value());
+	private final JsonSet<K, V, DataStructure<K>> jsonSet = new JsonSet<>(key(), value());
+	private final RpushAll<K, V, DataStructure<K>> rpush = new RpushAll<>(key(), value());
+	private final SaddAll<K, V, DataStructure<K>> sadd = new SaddAll<>(key(), value());
+	private final ZaddAll<K, V, DataStructure<K>> zadd = new ZaddAll<>(key(), value());
+	private final TsAddAll<K, V, DataStructure<K>> tsAdd = new TsAddAll<>(key(), value(), tsAddOptions());
 	private XAddAll<K, V, DataStructure<K>> xadd = xadd();
 
 	public void setMergePolicy(MergePolicy mergePolicy) {
 		this.mergePolicy = mergePolicy;
+	}
+
+	private static <K, V> AddOptions<K, V> tsAddOptions() {
+		Builder<K, V> builder = AddOptions.builder();
+		return builder.build();
+	}
+
+	private static <K> Function<DataStructure<K>, K> key() {
+		return DataStructure::getKey;
+	}
+
+	private static <K, T> Function<DataStructure<K>, T> value() {
+		return DataStructure::getValue;
 	}
 
 	public void setStreamIdPolicy(StreamIdPolicy streamIdPolicy) {
@@ -72,19 +77,27 @@ public class DataStructureWriteOperation<K, V> implements BatchOperation<K, V, D
 				continue;
 			}
 			if (shouldDelete(item)) {
-				futures.add(delete(commands, item));
+				futures.add((Future) delete(commands, item));
 			} else {
 				if (mergePolicy == MergePolicy.OVERWRITE && !DataStructure.STRING.equals(item.getType())) {
-					futures.add(delete(commands, item));
+					futures.add((Future) delete(commands, item));
 				}
-				futures.add((RedisFuture) operation(item).execute(commands, item));
-				if (item.getTtl() != null && item.getTtl() > 0) {
-					futures.add((RedisFuture) ((RedisKeyAsyncCommands<K, V>) commands).pexpireat(item.getKey(),
-							item.getTtl()));
+				futures.add((Future) operation(item).execute(commands, item));
+				if (shouldExpire(item)) {
+					futures.add((Future) expire(commands, item));
 				}
 			}
 		}
 		return futures;
+	}
+
+	private boolean shouldExpire(DataStructure<K> item) {
+		return item.getTtl() != null && item.getTtl() > 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	private RedisFuture<Boolean> expire(BaseRedisAsyncCommands<K, V> commands, DataStructure<K> item) {
+		return ((RedisKeyAsyncCommands<K, V>) commands).pexpireat(item.getKey(), item.getTtl());
 	}
 
 	private boolean shouldDelete(DataStructure<K> item) {
@@ -92,9 +105,9 @@ public class DataStructureWriteOperation<K, V> implements BatchOperation<K, V, D
 				|| DataStructure.isNone(item);
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	protected RedisFuture<Object> delete(BaseRedisAsyncCommands<K, V> commands, DataStructure<K> item) {
-		return (RedisFuture) ((RedisKeyAsyncCommands<K, V>) commands).del(item.getKey());
+	@SuppressWarnings("unchecked")
+	protected RedisFuture<Long> delete(BaseRedisAsyncCommands<K, V> commands, DataStructure<K> item) {
+		return ((RedisKeyAsyncCommands<K, V>) commands).del(item.getKey());
 	}
 
 	private Operation<K, V, DataStructure<K>, ?> operation(DataStructure<K> item) {
