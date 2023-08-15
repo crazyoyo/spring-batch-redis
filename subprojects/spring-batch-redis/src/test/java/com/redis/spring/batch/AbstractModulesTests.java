2668e7bb1434d101c6f1d26e477d046b1f45bbc4
Julien Ruaux <jruaux@users.noreply.github.com>
2023-08-15 10:03:29 -0700

    test: Fixed jsonSet

--- subprojects/spring-batch-redis/src/test/java/com/redis/spring/batch/AbstractModulesTests.java
+++ subprojects/spring-batch-redis/src/test/java/com/redis/spring/batch/AbstractModulesTests.java
@@ -53,7 +53,7 @@ import io.lettuce.core.codec.StringCodec;
 
 abstract class AbstractModulesTests extends AbstractTargetTests {
 
-    private static final String JSON_BEER_1 = "{\"id\":\"1\",\"brewery_id\":\"812\",\"name\":\"Hocus Pocus\",\"abv\":\"4.5\",\"ibu\":\"0\",\"srm\":\"0\",\"upc\":\"0\",\"filepath\":\"\",\"descript\":\"Our take on a classic summer ale.  A toast to weeds, rays, and summer haze.  A light, crisp ale for mowing lawns, hitting lazy fly balls, and communing with nature, Hocus Pocus is offered up as a summer sacrifice to clodless days.\\n\\nIts malty sweetness finishes tart and crisp and is best apprediated with a wedge of orange.\",\"add_user\":\"0\",\"last_mod\":\"2010-07-22 20:00:20 UTC\",\"style_name\":\"Light American Wheat Ale or Lager\",\"cat_name\":\"Other Style\"}";
+    private static final String JSON_BEER_1 = "[{\"id\":\"1\",\"brewery_id\":\"812\",\"name\":\"Hocus Pocus\",\"abv\":\"4.5\",\"ibu\":\"0\",\"srm\":\"0\",\"upc\":\"0\",\"filepath\":\"\",\"descript\":\"Our take on a classic summer ale.  A toast to weeds, rays, and summer haze.  A light, crisp ale for mowing lawns, hitting lazy fly balls, and communing with nature, Hocus Pocus is offered up as a summer sacrifice to clodless days.\\n\\nIts malty sweetness finishes tart and crisp and is best apprediated with a wedge of orange.\",\"add_user\":\"0\",\"last_mod\":\"2010-07-22 20:00:20 UTC\",\"style_name\":\"Light American Wheat Ale or Lager\",\"cat_name\":\"Other Style\"}]";
 
     private static final int BEER_COUNT = 1019;
 
@@ -103,7 +103,7 @@ abstract class AbstractModulesTests extends AbstractTargetTests {
         run(testInfo, reader, writer);
         Assertions.assertEquals(BEER_COUNT, sourceConnection.sync().keys("beer:*").size());
         Assertions.assertEquals(new ObjectMapper().readTree(JSON_BEER_1),
-                new ObjectMapper().readTree(sourceConnection.sync().jsonGet("beer:1")));
+                new ObjectMapper().readTree(sourceConnection.sync().jsonGet("beer:1", "$")));
     }
 
     @Test
