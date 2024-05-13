994995e13aa23dcfa761d26e22139422b36c7403
jruaux <jruaux@gmail.com>
2024-05-13 11:45:06 -0700

    refactor!: Removed Job factory

---
+++ subprojects/spring-batch-redis-infrastructure/src/main/java/com/redis/spring/batch/JobUtils.java
@@ -0,0 +1,76 @@
+package com.redis.spring.batch;
+
+import java.util.Optional;
+
+import org.hsqldb.jdbc.JDBCDataSource;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.boot.autoconfigure.batch.BatchDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc;
+import org.springframework.boot.jdbc.init.DataSourceScriptDatabaseInitializer;
+import org.springframework.boot.sql.init.DatabaseInitializationMode;
+
+public abstract class JobUtils {
+
+	private static final String STEP_ERROR = "Error executing step %s: %s";
+
+	private JobUtils() {
+	}
+
+	public static ResourcelessTransactionManager resourcelessTransactionManager() {
+		return new ResourcelessTransactionManager();
+	}
+
+	public static JobRepositoryFactoryBean jobRepositoryFactoryBean() throws Exception {
+		JobRepositoryFactoryBean bean = new JobRepositoryFactoryBean();
+		bean.setDataSource(hsqldbDataSource("redis"));
+		bean.setDatabaseType("HSQL");
+		bean.setTransactionManager(resourcelessTransactionManager());
+		bean.afterPropertiesSet();
+		return bean;
+	}
+
+	public static JDBCDataSource hsqldbDataSource(String databaseName) throws Exception {
+		JDBCDataSource source = new JDBCDataSource();
+		source.setURL("jdbc:hsqldb:mem:" + databaseName);
+		Jdbc jdbc = new Jdbc();
+		jdbc.setInitializeSchema(DatabaseInitializationMode.ALWAYS);
+		DataSourceScriptDatabaseInitializer initializer = new BatchDataSourceScriptDatabaseInitializer(source, jdbc);
+		initializer.afterPropertiesSet();
+		initializer.initializeDatabase();
+		return source;
+	}
+
+	public static JobExecution checkJobExecution(JobExecution jobExecution) throws JobExecutionException {
+		if (isFailed(jobExecution.getExitStatus())) {
+			Optional<JobExecutionException> stepExecutionException = jobExecution.getStepExecutions().stream()
+					.filter(e -> isFailed(e.getExitStatus())).map(JobUtils::stepExecutionException).findAny();
+			if (stepExecutionException.isPresent()) {
+				throw stepExecutionException.get();
+			}
+			if (jobExecution.getAllFailureExceptions().isEmpty()) {
+				throw new JobExecutionException(String.format("Error executing job %s: %s",
+						jobExecution.getJobInstance(), jobExecution.getExitStatus().getExitDescription()));
+			}
+		}
+		return jobExecution;
+	}
+
+	private static JobExecutionException stepExecutionException(StepExecution stepExecution) {
+		String message = String.format(STEP_ERROR, stepExecution.getStepName(),
+				stepExecution.getExitStatus().getExitDescription());
+		if (stepExecution.getFailureExceptions().isEmpty()) {
+			return new JobExecutionException(message);
+		}
+		return new JobExecutionException(message, stepExecution.getFailureExceptions().get(0));
+	}
+
+	private static boolean isFailed(ExitStatus exitStatus) {
+		return exitStatus.getExitCode().equals(ExitStatus.FAILED.getExitCode());
+	}
+
+}
